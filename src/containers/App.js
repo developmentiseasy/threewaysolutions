ed6b4c0f84145bcd1d38be4ef80916785ee6bf3d
Dmitriy Barchuk <dmitriy.barchuk@introlab-systems.com>
2017-07-26 19:48:43 +0300

    Add animated bg

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-import './App.css'
-
-import logo from '../common/logo.svg'
-import User from '../components/User'
-import Page from '../components/Page'
 import { bindActionCreators } from 'redux'
 import * as pageActions from '../actions/page/PageActions'
+
+import User from '../components/User'
+import Page from '../components/Page'
+import ParticlesHeader from '../components/ParticlesHeader/ParticlesHeader'
+
+import logo from '../common/logo.svg'
+
+import './App.css'
 
 class App extends Component {
 
@@ -16,14 +19,15 @@ class App extends Component {
     const { getPhotos } = this.props.pageActions
 
     return (
-      <div className="App container h-100">
-        <div>
-          <img className="logo" src={logo} alt="" />
-        </div>
-        <div>
-          <User name={user.name} />
-          <Page photos={page.photos} year={page.year} getPhotos={getPhotos} fetching={page.fetching}/>
-        </div>
+      <div className="App">
+        <ParticlesHeader/>
+        {/*<div>*/}
+          {/*<img className="logo" src={logo} alt="" />*/}
+        {/*</div>*/}
+        {/*<div>*/}
+          {/*<User name={user.name} />*/}
+          {/*<Page photos={page.photos} year={page.year} getPhotos={getPhotos} fetching={page.fetching}/>*/}
+        {/*</div>*/}
       </div>
     )
   }

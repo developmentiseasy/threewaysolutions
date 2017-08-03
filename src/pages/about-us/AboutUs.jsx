f7a73cc5c4d65ec853cf68c7c107931d721b7ac9
Dmitiy Barchuk <developmentiseasy@gmail.com>
2017-08-03 08:59:08 +0300

    Implement About Us Title fo AboutUs page

---
+++ src/pages/about-us/AboutUs.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+import './AboutUs.css'
+
+export default class AboutUs extends Component {
+  render() {
+    return (
+      <div className="AboutUs container">
+        <div className="card-title">
+          <div className="title-about">About</div>
+          <div className="title-us">Us</div>
+        </div>
+        <div className="about-us-card">
+
+        </div>
+      </div>
+    )
+  }
+}

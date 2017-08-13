5854f1d9c610cade6188b5f815a968743fadfbb5
Sergey Timechko <sergey.timechko@threewaysolutions.com>
2017-08-13 11:11:37 +0300

    TWS-10 Same refactoring code. Implement first card view.

--- src/pages/about-us/AboutUs.jsx
+++ src/pages/about-us/AboutUs.jsx
@@ -4,35 +4,33 @@ import './AboutUs.css'
 
 export default class AboutUs extends Component {
 
-  sameFunc = () => {
-
-  }
-
   render() {
     return (
       <div className="AboutUs container">
-        <div className="card-title">
+        <div className="about-us-title">
           <div className="title-about">About</div>
           <div className="title-us">Us</div>
         </div>
-        <div className="col-md-12 about-us-cards">
-          <div className="row cards">
-            <div className="block-of-cards col-md-4">
-              <h3 className="we_are"> WE ARE </h3>
-              <span className="creative">creative</span>
-              <p className="text_lorem">Lorem ipsum dolor sit amet,
+        <div className="col-md-12 cards">
+          <div className="row cards-row">
+            <div className="description-card col-md-4">
+              <div className="card-title">
+                <h3 className="title-thin"> WE ARE </h3>
+                <span className="subtitle-bold">creative</span>
+              </div>
+              <p className="card-context">Lorem ipsum dolor sit amet,
                 consectetur adipisicing elit. A architecto culpa dicta distinctio doloremque,
                 earum error esse est ex expedita fuga fugiat illum ipsum magni molestiae nulla
-                odio odit officia placeat quo quod repudiandae soluta, tempore veniam.
+                odio odit officia placeat quo quod repudiandae soluta, tempore veniam
                 <span className="his">his</span></p>
             </div>
-            <div className="block-of-cards col-md-4">We are ambitions</div>
-            <div className="block-of-cards col-md-4">We are ambitions.</div>
+            <div className="description-card col-md-4">2</div>
+            <div className="description-card col-md-4">3</div>
           </div>
-          <div className="row cards">
-            <div className="block-of-cards col-md-4">4</div>
-            <div className="block-of-cards col-md-4">5</div>
-            <div className="block-of-cards col-md-4">6</div>
+          <div className="row cards-row">
+            <div className="description-card col-md-4">4</div>
+            <div className="description-card col-md-4">5</div>
+            <div className="description-card col-md-4">6</div>
           </div>
         </div>
       </div>

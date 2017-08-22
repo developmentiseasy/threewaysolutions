c22bbee2edbd43c80712e0b1f6ccf8cd982057d4
Sergey Timechko <sergey.timechko@threewaysolutions.com>
2017-08-22 23:02:01 +0300

    Assigned styles to three cards

--- src/pages/about-us/components/cards/WeAreInitiativeCard.jsx
+++ src/pages/about-us/components/cards/WeAreInitiativeCard.jsx
@@ -10,13 +10,16 @@ export default class WeAreInitiativeCard extends Component {
           <h3 className="title-thin"> WE ARE </h3>
           <span className="subtitle-bold">initiative</span>
         </div>
-        <p className="gray-boarder card-context">Lorem ipsum dolor sit amet,
-          consectetur adipisicing elit. Ab alias aliquam blanditiis dolores et,
-          fugiat ipsum iste maiores possimus quis quod sit tempore tenetur
-          totam voluptate? Ad dicta excepturi illo, minima natus pariatur
-          quidem repellat sequi tempora unde.
-          <span className="his"> his </span></p>
+        <div className="gray-boarder">
+          <p className=" card-context">Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Ab alias aliquam blanditiis dolores et,
+            fugiat ipsum iste maiores possimus quis quod sit tempore tenetur
+            totam voluptate? Ad dicta excepturi illo, minima natus pariatur
+            quidem repellat sequi tempora unde.
+            <span className="his"> his </span></p>
+        </div>
       </div>
+
     )
   }
 }
